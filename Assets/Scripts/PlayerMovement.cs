3da17ada1ee739c230a9f92f27be19893ae4bd3f
SeaCow7 <daniel.marker@gmail.com>
2019-08-02 23:52:47 +0100

    Created a Track class and created a path image for the test track. Also updated playermovement so that it checks if the player position on the path image is a black pixel and kills the player if it is

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float playerSpeed = 2f;
     [SerializeField] float rotationSpeed = 2f;
 
+    Track currentTrack;
     bool moving = false;
     bool moveLeft = false;
+    bool alive = true;
+
+    const float a = 1440f/13.332f;
+    const float b = 108f;
+    const float c = 960f;
+    const float d = 540f;
+
+    void Start() {
+        currentTrack = FindObjectOfType<Track>();
+    }
 
     void Update()
     {
-        if (moving)
+        if (moving && alive)
         {
             Vector3 movementVector = new Vector3();
             float rotationAngle = 0f;
@@ -28,21 +39,32 @@ public class PlayerMovement : MonoBehaviour
             movementVector *= playerSpeed / 1000f;
 
             gameObject.transform.Rotate(0, 0, rotationAngle, Space.Self);
-            //gameObject.transform.Rotate(rotationVector,Space.Self);
             gameObject.transform.Translate(movementVector, Space.World);
         }
+
+        Vector2 currPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        Vector2Int imgPos = new Vector2Int(Mathf.RoundToInt(a * currPos.x + c), Mathf.RoundToInt(b * currPos.y + d));
+        Color posColor = currentTrack.pathImg.GetPixel(imgPos.x, imgPos.y);
+        if (posColor == Color.black)
+        {
+            PlayerDeath();
+        }
+    }
+
+    private void PlayerDeath()
+    {
+        print("Dead");
+        alive = false;
     }
 
     public void Left()
     {
-        print("left");
         moving = true;
         moveLeft = true;
     }
 
     public void Right()
     {
-        print("right");
         moving = true;
         moveLeft = false;
     }
