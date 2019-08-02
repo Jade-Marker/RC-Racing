255223317d34d69b9635f9b84bf8252a1a323442
SeaCow7 <daniel.marker@gmail.com>
2019-08-02 22:21:55 +0100

    Created car sprite and player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 2f;
+    [SerializeField] float rotationSpeed = 2f;
+
+    bool moving = false;
+    bool moveLeft = false;
+
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 movementVector = new Vector3();
+            float rotationAngle = 0f;
+
+            if (moveLeft) {
+                movementVector = gameObject.transform.up;
+                rotationAngle = rotationSpeed * Time.deltaTime;
+            }
+            else {
+                movementVector = gameObject.transform.up;
+                rotationAngle = -rotationSpeed * Time.deltaTime;
+            }
+            movementVector *= playerSpeed / 1000f;
+
+            gameObject.transform.Rotate(0, 0, rotationAngle, Space.Self);
+            //gameObject.transform.Rotate(rotationVector,Space.Self);
+            gameObject.transform.Translate(movementVector, Space.World);
+        }
+    }
+
+    public void Left()
+    {
+        print("left");
+        moving = true;
+        moveLeft = true;
+    }
+
+    public void Right()
+    {
+        print("right");
+        moving = true;
+        moveLeft = false;
+    }
+}
