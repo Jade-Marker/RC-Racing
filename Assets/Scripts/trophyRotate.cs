c434447cb459333dcf470191c67963c647167f11
SeaCow7 <daniel.marker@gmail.com>
2019-08-04 17:10:51 +0100

    Created ending and added a trophy with a script to rotate it. Made level 2 & an alt track piece for "idk". Also swapped player and enemy position in level 1

---
+++ Assets/Scripts/trophyRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trophyRotate : MonoBehaviour
+{
+    [SerializeField] float degPs = 5f;
+    
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, degPs * Time.deltaTime);
+    }
+}
